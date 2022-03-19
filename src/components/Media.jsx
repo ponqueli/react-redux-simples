e7a2eb13f15d7e135f495f3fb5a477018a2392b4
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-03-18 22:19:36 -0300

    conectando componentes com redux

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,18 +1,30 @@
-import './Intervalo.css';
-import Card from './Card';
-import React from 'react';
+import "./Intervalo.css";
+import Card from "./Card";
+import React from "react";
+import { connect } from "react-redux"; //does the connection between component and state
 
 const Media = (props) => {
+  const { min, max } = props;
+  console.log(props)
   return (
     <Card title="Média de Números" green>
-        <div className="Media">
-            <span>
-                <span>Resultado:</span>
-                <strong>5</strong>
-            </span>
-        </div>
+      <div className="Media">
+        <span>
+          <span>Resultado:</span>
+          <strong>{(max + min) / 2}</strong>
+        </span>
+      </div>
     </Card>
-  )
+  );
+};
+
+//estado global de todos os componentes
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+    nomes: state.nomes
+  };
 }
 
-export default Media;
+export default connect(mapStateToProps)(Media);
