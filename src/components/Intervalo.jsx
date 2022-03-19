dc966f642838bf4e710f601fe5ddc7513e7de9d7
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-03-19 18:55:24 -0300

    criando action creator, action e dispatch quando muda min e max

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,3 +1,4 @@
+import { alterarNumeroMinimo, alterarNumeroMaximo } from "../store/actions/numeros";
 import "./Intervalo.css";
 import Card from "./Card";
 import React from "react";
@@ -5,16 +6,17 @@ import { connect } from "react-redux";
 
 const Intervalo = (props) => {
   const { min, max } = props;
+  
   return (
     <Card title="Intervalo de Números" red>
       <div className="Intervalo">
         <span>
           <strong>Mínimo:</strong>
-          <input type="number" value={min} readOnly />
+          <input type="number" value={min} onChange={e=> props.alterarMinimo(+e.target.value)} />
         </span>
         <span>
           <strong>Máximo:</strong>
-          <input type="number" value={max} readOnly />
+          <input type="number" value={max} onChange={e=>props.alterarMaximo(+e.target.value)} />
         </span>
       </div>
     </Card>
@@ -28,4 +30,22 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Intervalo);
+//chamar o dispatch que acabou de acontecer e notificar todos os reducers da aplicação
+function mapDispatchToProp(dispatch) {
+  return {
+    alterarMinimo(novoNumero){
+      //action creator -> retorna uma action
+      const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+    alterarMaximo(novoNumero){
+      const action = alterarNumeroMaximo(novoNumero);
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(
+    mapStateToProps,          // 1o estado das propriedades
+    mapDispatchToProp         // 2o action
+    )(Intervalo);
